023a41efceab57445e5f24b15464232a34797996
shystruk <v.stokol@gmail.com>
2020-12-12 10:15:27 -0800

    formatting

--- src/SetInterval.js
+++ src/SetInterval.js
@@ -3,55 +3,53 @@
  License: MIT - https://opensource.org/licenses/MIT
  https://github.com/shystruk/SetIntervalJS
  */
-(function(f) {
-    if (typeof exports === 'object' && typeof module !== 'undefined') {
-        module.exports = f();
-    } else if (typeof define === 'function' && define.amd) {
-        define([],f);
-    } else {
-        var g;
+(function (f) {
+	if (typeof exports === 'object' && typeof module !== 'undefined') {
+		module.exports = f();
+	} else if (typeof define === 'function' && define.amd) {
+		define([], f);
+	} else {
+		var g;
 
-        if (typeof window !== 'undefined') {
-            g = window;
-        } else if(typeof global !== 'undefined') {
-            g = global;
-        } else if(typeof self !== 'undefined') {
-            g = self;
-        } else {
-            g = this;
-        }
+		if (typeof window !== 'undefined') {
+			g = window;
+		} else if (typeof global !== 'undefined') {
+			g = global;
+		} else if (typeof self !== 'undefined') {
+			g = self;
+		} else {
+			g = this;
+		}
 
-        g.myModule = f();
-    }
-})(function() {
-    var define,module,exports;
+		g.myModule = f();
+	}
+})(function () {
+	function SetInterval() {
+		this.key = {};
 
-    function SetInterval() {
-        this.key = {};
+		/**
+		 * @param {Function} fn
+		 * @param {Number} interval
+		 * @param {String} key
+		 */
+		this.start = function start(fn, interval, key) {
+			if (!this.key[key]) {
+				this.key[key] = setInterval(function () {
+					fn();
+				}, interval);
+			}
+		}
 
-        /**
-         * @param {Function} fn 
-         * @param {Number} interval 
-         * @param {String} key 
-         */
-        this.start = function start(fn, interval, key) { 
-            if (!this.key[key]) {
-                this.key[key] = setInterval(function () {
-                    fn();
-                }, interval);
-            }
-        }
+		/**
+		 * @param {String} key
+		 */
+		this.clear = function clear(key) {
+			if (this.key[key]) {
+				clearInterval(this.key[key]);
+				delete this.key[key];
+			}
+		}
+	}
 
-        /**
-         * @param {String} key 
-         */
-        this.clear = function clear(key) {
-            if (this.key[key]) {
-                clearInterval(this.key[key]);
-                delete this.key[key];
-            }
-        }
-    }
-
-    return new SetInterval();
+	return new SetInterval();
 });
--- test.js
+++ test.js
@@ -2,57 +2,56 @@ import test from 'ava';
 import SetInterval from './src/SetInterval';
 
 test.cb('start interval', t => {
-    function callback() {
-        t.true(true);
-        t.is(typeof SetInterval.key['test'], number);
-    }
+	function callback() {
+		t.true(true);
+		t.is(typeof SetInterval.key['test'], number);
+	}
 
-    SetInterval.start(callback, 1000, 'test');
+	SetInterval.start(callback, 1000, 'test');
 
-    setTimeout(t.end);
+	setTimeout(t.end);
 });
 
 test.cb('clear interval', t => {
-    function callback() {
-        t.true(true);
-    }
+	function callback() {
+		t.true(true);
+	}
 
-    SetInterval.start(callback, 1000, 'test');
-    SetInterval.clear('test');
+	SetInterval.start(callback, 1000, 'test');
+	SetInterval.clear('test');
 
-    t.is(SetInterval.key['test'], void 0);
+	t.is(SetInterval.key['test'], void 0);
 
-    setTimeout(t.end);
+	setTimeout(t.end);
 });
 
 test.cb('multiple instances', t => {
-    function callback() {
-        t.true(true);
-    }
+	function callback() {
+		t.true(true);
+	}
 
-    function callback_2() {
-        t.true(true);
-    }
+	function callback_2() {
+		t.true(true);
+	}
 
-    function callback_3() {
-        t.true(true);
-        t.is(typeof SetInterval.key['test'], number);
-        t.is(typeof SetInterval.key['test_2'], number);
-        t.is(typeof SetInterval.key['test_3'], number);
-    }
+	function callback_3() {
+		t.true(true);
+		t.is(typeof SetInterval.key['test'], number);
+		t.is(typeof SetInterval.key['test_2'], number);
+		t.is(typeof SetInterval.key['test_3'], number);
+	}
 
-    SetInterval.start(callback, 1000, 'test');
-    SetInterval.start(callback_2, 2000, 'test_2');
-    SetInterval.start(callback_3, 3000, 'test_3');
+	SetInterval.start(callback, 1000, 'test');
+	SetInterval.start(callback_2, 2000, 'test_2');
+	SetInterval.start(callback_3, 3000, 'test_3');
 
-    SetInterval.clear('test');
-    SetInterval.clear('test_2');
-    SetInterval.clear('test_3');
+	SetInterval.clear('test');
+	SetInterval.clear('test_2');
+	SetInterval.clear('test_3');
 
-    t.is(SetInterval.key['test'], void 0);
-    t.is(SetInterval.key['test_2'], void 0);
-    t.is(SetInterval.key['test_3'], void 0);
+	t.is(SetInterval.key['test'], void 0);
+	t.is(SetInterval.key['test_2'], void 0);
+	t.is(SetInterval.key['test_3'], void 0);
 
-    setTimeout(t.end);
+	setTimeout(t.end);
 });
-
